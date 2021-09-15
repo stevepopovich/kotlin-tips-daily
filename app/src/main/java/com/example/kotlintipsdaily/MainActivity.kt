eca00ea3e25e79e118efbb7cd013bb213b547c63
Steven Personal <stevepopovich8@gmail.com>
2021-09-14 22:46:59 -0400

    remove a bunch of shit and add compose

--- app/src/main/java/com/example/kotlintipsdaily/MainActivity.kt
+++ app/src/main/java/com/example/kotlintipsdaily/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.example.kotlintipsdaily
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.kotlintipsdaily.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlintipsdaily.ui.theme.KotlinTipsDailyTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivityCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            KotlinTipsDailyTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KotlinTipsDailyTheme {
+        Greeting("Android")
     }
 }
